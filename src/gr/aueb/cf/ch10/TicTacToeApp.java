eb717078849245eb24586ef45752e8e33122fb6d
Spiros Natsios <spi.natsios@gmail.com>
2024-06-06 17:55:44 +0300

    Changed the way it calculated the winner

--- src/gr/aueb/cf/ch10/TicTacToeApp.java
+++ src/gr/aueb/cf/ch10/TicTacToeApp.java
@@ -3,11 +3,9 @@ package gr.aueb.cf.ch10;
 import java.util.Scanner;
 
 public class TicTacToeApp {
-    static int playerXsum = 0;
-    static int playerOsum = 0;
-    static char[] grid = { '7', '8', '9', '4', '5', '6', '1', '2', '3' };
+    static char[] grid = {'7', '8', '9', '4', '5', '6', '1', '2', '3'};
     static char currentPlayer = 'X';
-    static boolean gridUpdated;
+    static boolean gridUpdated = false;
     static int countPlays = 0;
 
     public static void main(String[] args) {
@@ -20,12 +18,10 @@ public class TicTacToeApp {
             int choice = readFromUser(scanner);
             updateGrid(choice);
             if (gridUpdated) {
-                addScoreToVariables(choice);
                 if (checkWinner()) {
                     isPlaying = false;
                 } else {
                     switchPlayer();
-                    gridUpdated = false;
                 }
             }
         } while (isPlaying);
@@ -33,32 +29,26 @@ public class TicTacToeApp {
         announceResults();
     }
 
-    static boolean checkWinner() {
-        if (countPlays > 4) {
-            if (currentPlayer == 'X') {
-                if ((playerXsum % 6 == 0) || playerXsum == 15) {
-                    return true;
-                }
-            } else {
-                if ((playerOsum % 6 == 0) || playerOsum == 15) {
-                    return true;
-                }
+    public static boolean checkWinner() {
+        for (int i = 0; i < 9; i += 3) {
+            if (grid[i] == currentPlayer && grid[i + 1] == currentPlayer && grid[i + 2] == currentPlayer) {
+                return true;
             }
+        }
+        for (int i = 0; i < 3; i++) {
+            if (grid[i] == currentPlayer && grid[i + 3] == currentPlayer && grid[i + 6] == currentPlayer) {
+                return true;
+            }
+        }
+        if ((grid[0] == currentPlayer && grid[4] == currentPlayer && grid[8] == currentPlayer) ||
+                (grid[2] == currentPlayer && grid[4] == currentPlayer && grid[6] == currentPlayer)) {
+            return true;
         }
         return countPlays == 9;
     }
 
-    static void addScoreToVariables(int choice) {
-        if (currentPlayer == 'X') {
-            playerXsum += choice;
-            System.out.println(playerXsum);
-        } else {
-            playerOsum += choice;
-            System.out.println(playerOsum);
-        }
-    }
 
-    static void showGrid() {
+    public static void showGrid() {
         System.out.println("\t|------------|");
         System.out.println("\t   |" + grid[0] + " " + grid[1] + " " + grid[2] + "|");
         System.out.println("\t   |" + grid[3] + " " + grid[4] + " " + grid[5] + "|");
@@ -66,7 +56,7 @@ public class TicTacToeApp {
         System.out.println("\t|------------|");
     }
 
-    static void updateGrid(int choice) {
+    public static void updateGrid(int choice) {
         char c = (char) (choice + '0');
         for (int i = 0; i < grid.length; i++) {
             if (grid[i] == c) {
@@ -80,13 +70,13 @@ public class TicTacToeApp {
         gridUpdated = false;
     }
 
-    static int readFromUser(Scanner scanner) {
-        String input = "";
-        int result = 0;
+    public static int readFromUser(Scanner scanner) {
+        String input;
+        int result;
         System.out.println(currentPlayer + " Plays");
         input = scanner.nextLine();
         while (!input.matches("[1-9]")) {
-            System.out.println("please select an integer from 1 to 9");
+            System.out.println("Please select an integer from 1 to 9.");
             input = scanner.nextLine();
         }
         result = Integer.parseInt(input);
@@ -94,33 +84,22 @@ public class TicTacToeApp {
     }
 
     static void switchPlayer() {
-        if (gridUpdated) {
-            if (currentPlayer == 'X') {
-                currentPlayer = 'O';
-            } else {
-                currentPlayer = 'X';
-            }
-        }
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
     }
 
-    static void announceResults() {
+    public static void announceResults() {
+        System.out.println("[~][~][~][~][~][~][~][~][~][~]");
         if (countPlays == 9) {
-            System.out.println("[~][~][~][~][~][~][~][~][~][~]");
-            System.out.println("\t  It's a draw! ");
+            System.out.println("\tIt's a draw!");
             showGrid();
-            System.out.println("[~][~][~][~][~][~][~][~][~][~]");
         } else {
-            System.out.println("[~][~][~][~][~][~][~][~][~][~]");
-            System.out.println("\t Player " + currentPlayer + " Wins ");
+            System.out.println("\tPlayer " + currentPlayer + " wins!");
             showGrid();
-            System.out.println("[~][~][~][~][~][~][~][~][~][~]");
-
-            System.out.println("playerX :" + playerXsum);
-            System.out.println("playerO :" + playerOsum);
         }
+        System.out.println("[~][~][~][~][~][~][~][~][~][~]");
     }
 
-    static void clearScreen() {
+    public static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
